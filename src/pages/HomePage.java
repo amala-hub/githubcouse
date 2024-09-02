819adcd62df58b22de0ba0a00f4bd5dcfc55406f
Amala Alla <amalalla@in.ibm.com>
2024-09-02 19:19:52 +0530

    added/updated/deleted files

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -2,4 +2,12 @@ package pages;
 
 public class HomePage {
 
+	public static void main(String[] args) {
+        String str = "Hello, World!"; // Example string
+        String reversedStr = "";
+
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }
 }
